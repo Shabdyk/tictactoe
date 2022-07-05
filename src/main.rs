a951e4bfa17aad183d7b8e2ab3abcb69c9045cbd
shab <kydshab@gmail.com>
2022-07-06 01:04:45 +0600

    tested fn select(). working on fn show()

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ type Position = (usize, usize);
 fn main() {
     let mut ttt = Tictactoe::new(3, 3);
     // println!("{:?}", ttt);
-    Tictactoe::select(&mut ttt, 'x', (1, 2));
+    ttt.select('o', (1, 1));
     println!("{:?}", ttt);
 }
 
@@ -15,7 +15,7 @@ struct Tictactoe {
     height: usize,
     occup_fields: HashSet<Position>,
     x_fields: HashSet<Position>,
-    y_fields: HashSet<Position>,
+    o_fields: HashSet<Position>,
 }
 
 impl Tictactoe {
@@ -25,21 +25,30 @@ impl Tictactoe {
             height,
             occup_fields: HashSet::new(),
             x_fields: HashSet::new(),
-            y_fields: HashSet::new(),
+            o_fields: HashSet::new(),
         }
     }
-    pub fn select(&mut self, x_or_y: char, pos: Position) {
-        if self.width >= pos.0 && self.height <= pos.1 {
+    pub fn select(&mut self, x_or_o: char, pos: Position) {
+        if self.width >= pos.0 && self.height >= pos.1 {
             if !self.occup_fields.contains(&pos) {
-                if x_or_y == 'x' {
+                if x_or_o == 'x' {
                     self.x_fields.insert(pos);
-                } else if x_or_y == 'y' {
-                    self.y_fields.insert(pos);
+                } else if x_or_o == 'o' {
+                    self.o_fields.insert(pos);
                 } else {
-                    panic!("Type x or y!")
+                    panic!("Type x or o!")
                 }
             }
             self.occup_fields.insert(pos);
+        } else {
+            panic!("Position out of range!")
+        }
+    }
+    pub fn show(&mut self) {
+        for h in self.height {
+            for w in self.width {
+                println!("_");
+            }
         }
     }
 }
