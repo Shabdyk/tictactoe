2255183740269324bef364896d32fb6961633561
shab <kydshab@gmail.com>
2022-07-07 22:14:39 +0600

    implemented fn win() for big fields

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ use std::collections::HashSet;
 type Position = (usize, usize);
 
 fn main() {
-    let mut ttt = Tictactoe::new(3, 3);
+    let mut ttt = Tictactoe::new(10, 10);
     // println!("{:?}", ttt);
-    ttt.select('o', (0, 2));
-    ttt.select('o', (0, 1));
-    ttt.select('o', (2, 1));
-    ttt.select('o', (0, 0));
-    println!("{:?}", ttt);
+    ttt.select('o', (1, 3));
+    ttt.show();
+    ttt.select('o', (1, 4));
+    ttt.show();
+    ttt.select('x', (7, 1));
+    ttt.select('o', (1, 2));
+    ttt.show();
+    ttt.select('o', (1, 5));
+    ttt.select('o', (1, 6));
+    // println!("{:?}", ttt);
     ttt.show();
 }
 
@@ -40,13 +45,13 @@ impl Tictactoe {
                     self.x_fields.insert(pos);
                     //self.win(self.x_fields.clone());
                     if self.win(self.x_fields.clone()) {
-                         println!("X won!")
+                        println!("X won! {:?}", self.x_fields)
                     };
                 } else if x_or_o == 'o' {
                     self.o_fields.insert(pos);
-                       if self.win(self.o_fields.clone()) {
-                         println!("O won!")
-                         }
+                    if self.win(self.o_fields.clone()) {
+                        println!("O won! {:?}", self.o_fields)
+                    }
                 } else {
                     panic!("Type x or o!")
                 }
@@ -68,27 +73,51 @@ impl Tictactoe {
                         print!("[ O ]");
                     }
                 } else {
-                    print!("[{} {}]", h, w);
+                    // print!("[{} {}]", h, w);
+                    print!("[   ]");
                 }
             }
             println!("");
         }
+        println!("____________________")
     }
     fn win(&mut self, chk: HashSet<Position>) -> bool {
-        //Horizontal
-        let mut chk_h = Vec::new();
-        let mut bo_h = Vec::new();
-        for p in chk {
-            chk_h.push(p.0);
-            bo_h.push(chk_h.iter().filter(|&n| *n == p.0).count() == 3);
-            //println!("{:?}", chk_h.iter().filter(|&n| *n == p.0).count() == 3);
-            //println!("{:?}", &chk_h);
-            
+        //Horizontal/Vertical for 3x3
+        // let mut chk_h = Vec::new();
+        // let mut chk_v = Vec::new();
+        // let mut bools = Vec::new();
+        // for p in chk {
+        //     chk_h.push(p.0);
+        //     chk_v.push(p.1);
+        //     bools.push(chk_h.iter().filter(|&n| *n == p.0).count() == 3);
+        //     bools.push(chk_v.iter().filter(|&n| *n == p.1).count() == 3);
+        // }
+        // bools.contains(&true)
+        //
+        //Horizontal/Vertical for nxm
+        let mut res_h = false;
+        let mut res_v = false;
+        let mut res_d = false;
+        for (n, m) in chk.clone() {
+            res_v = chk.contains(&(n, m + 1))
+                && chk.contains(&(n, m + 2))
+                && chk.contains(&(n, m + 3))
+                && chk.contains(&(n, m + 4));
+            res_h = chk.contains(&(n + 1, m))
+                && chk.contains(&(n + 2, m))
+                && chk.contains(&(n + 3, m))
+                && chk.contains(&(n + 4, m));
+            res_d = chk.contains(&(n + 1, m + 1))
+                && chk.contains(&(n + 2, m + 2))
+                && chk.contains(&(n + 3, m + 3))
+                && chk.contains(&(n + 4, m + 4));
+            if res_v || res_h || res_d {
+                break;
+            }
+            // println!("{}/{}", n, m);
         }
-        
-        bo_h.contains(&true)
-        
-        // true
+        // println!("{:?} -> {}, {}, {}", chk, res_h, res_v, res_d);
+        res_h || res_v || res_d
     }
 }
 
